c7259c7b22d9ffb3a2d0e44ceae8c6398ae9111b
2muni <relesinc@gmail.com>
2019-09-03 12:23:15 +0900

    Add gatsby-ssr with Wrap Provider

---
+++ wrap-with-provider.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { ContextProvider } from './src/utils/Context'
+
+// eslint-disable-next-line
+export default ({ element }) => <ContextProvider>{element}</ContextProvider>
